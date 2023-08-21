f2463c767c715682539ae98a5cfe706c7d409b7a
José Ramírez <138532719+kuuukooo@users.noreply.github.com>
2023-08-21 08:48:21 -0400

    Add files via upload

---
+++ conexion_bd.php
@@ -0,0 +1,8 @@
+<?php
+$conex = mysqli_connect("192.168.12.37:3306", "Jose", "holahola", "login");
+$conex->set_charset("utf8");
+//if (mysqli_connect_errno()) {
+   // echo "Error al conectar con la base de datos: " . mysqli_connect_error();
+   // exit();
+//}
+?>
